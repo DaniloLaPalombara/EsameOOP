4c6299198ca7506a6a03aa2b9875c185cadfa2ba
NicoloIanni <nicoloianni01@gmail.com>
2021-12-10 12:10:50 +0100

    Implementazione getter e setter classe city

--- ESAMEOOP/src/main/java/it/univpm/ESAMEOOP/model/City.java
+++ ESAMEOOP/src/main/java/it/univpm/ESAMEOOP/model/City.java
@@ -9,8 +9,11 @@ public class City {
 	private double feels_like;
 	private double temp_max;
 	private double temp_min;
+	private double lat;
+	private double lon;
 	
-	public City (double id, String country, String name, double temp, double flike, double tmax, double tmin) {
+	
+	public City (double id, String country, String name, double temp, double flike, double tmax, double tmin,double lat,double lon) {
 		this.id = id;
 		this.country = country;
 		this.name = name;
@@ -18,6 +21,100 @@ public class City {
 		this.feels_like = flike;
 		this.temp_max = tmax;
 		this.temp_min = tmin;	
+		this.lat=lat;
+		this.lon=lon;
 	}
 
+
+	public double getId() {
+		return id;
+	}
+
+
+	public void setId(double id) {
+		this.id = id;
+	}
+
+
+	public String getCountry() {
+		return country;
+	}
+
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public double getTemp() {
+		return temp;
+	}
+
+
+	public void setTemp(double temp) {
+		this.temp = temp;
+	}
+
+
+	public double getFeels_like() {
+		return feels_like;
+	}
+
+
+	public void setFeels_like(double feels_like) {
+		this.feels_like = feels_like;
+	}
+
+
+	public double getTemp_max() {
+		return temp_max;
+	}
+
+
+	public void setTemp_max(double temp_max) {
+		this.temp_max = temp_max;
+	}
+
+
+	public double getTemp_min() {
+		return temp_min;
+	}
+
+
+	public void setTemp_min(double temp_min) {
+		this.temp_min = temp_min;
+	}
+
+
+	public double getLat() {
+		return lat;
+	}
+
+
+	public void setLat(double lat) {
+		this.lat = lat;
+	}
+
+
+	public double getLon() {
+		return lon;
+	}
+
+
+	public void setLon(double lon) {
+		this.lon = lon;
+	}
+	
+
+	
 }
