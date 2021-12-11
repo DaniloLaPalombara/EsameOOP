e8296bb47cf974f61e3bde5d2a0f15a43983a62f
NicoloIanni <nicoloianni01@gmail.com>
2021-12-11 17:08:16 +0100

    Implementazione metodi interfaccia

--- ESAMEOOP/src/main/java/it/univpm/ESAMEOOP/service/OpenWeatherInterface.java
+++ ESAMEOOP/src/main/java/it/univpm/ESAMEOOP/service/OpenWeatherInterface.java
@@ -1,6 +1,12 @@
 package it.univpm.ESAMEOOP.service;
 
-public class OpenWeatherInterface {
+import it.univpm.ESAMEOOP.model.City;
+
+public interface OpenWeatherInterface {
 	//TODO creare metodi per prendere le previsioni da API e creare JSON parametri meteo astratti
+	private abstract JSONObject createJSON(City city);
+	private abstract JSONObject getDataWeather(long id);
+	private abstract City setDataWeather (JSONObject dataWeather);
+	private abstract void saveFile(JSONObject obj);
 
 }
