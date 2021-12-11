e8540254173dfb5fef4d7f734e7cd1257ac86691
DaniloLP01 <danilola.palombara@gmail.com>
2021-12-11 17:13:21 +0100

    aggiunta import

--- ESAMEOOP/src/main/java/it/univpm/ESAMEOOP/service/OpenWeatherInterface.java
+++ ESAMEOOP/src/main/java/it/univpm/ESAMEOOP/service/OpenWeatherInterface.java
@@ -1,12 +1,13 @@
 package it.univpm.ESAMEOOP.service;
 
+import org.json.simple.*;
 import it.univpm.ESAMEOOP.model.City;
 
 public interface OpenWeatherInterface {
 	//TODO creare metodi per prendere le previsioni da API e creare JSON parametri meteo astratti
-	private abstract JSONObject createJSON(City city);
-	private abstract JSONObject getDataWeather(long id);
-	private abstract City setDataWeather (JSONObject dataWeather);
-	private abstract void saveFile(JSONObject obj);
+	public abstract JSONObject createJSON(City city);
+	public abstract JSONObject getDataWeather(long id);
+	public abstract City setDataWeather (JSONObject dataWeather);
+	public abstract void saveFile(JSONObject obj);
 
 }
