2c5f5e47b4f3c8dd108314bcca7728e1dbc67199
DaniloLP01 <danilola.palombara@gmail.com>
2021-12-11 13:14:51 +0100

    Modifica rotta

--- ESAMEOOP/src/main/java/it/univpm/ESAMEOOP/controller/OpenWeatherController.java
+++ ESAMEOOP/src/main/java/it/univpm/ESAMEOOP/controller/OpenWeatherController.java
@@ -1,5 +1,12 @@
 package it.univpm.ESAMEOOP.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class OpenWeatherController {
+	@GetMapping(///"Nome rotta")
+			public //oggetto
+			
+	
 
 }
