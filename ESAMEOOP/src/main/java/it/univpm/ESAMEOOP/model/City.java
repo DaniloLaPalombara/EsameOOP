13497ec037ff700e44e90db576c4207e392d45c3
DaniloLP01 <danilola.palombara@gmail.com>
2021-12-14 12:23:46 +0100

    Modifica classe

--- ESAMEOOP/src/main/java/it/univpm/ESAMEOOP/model/City.java
+++ ESAMEOOP/src/main/java/it/univpm/ESAMEOOP/model/City.java
@@ -1,23 +1,36 @@
 package it.univpm.ESAMEOOP.model;
 
 import java.util.Vector;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
 
+import javax.persistence.Id;
+
+@Entity
+
+@Table(name = "Weather")
 public class City {
+	
+	@Id
+	@GeneretedValue(strategy = GenerationType.AUTO);
 	
 	private long id;
 	private String country;
 	private String name;
 	private double lat;
 	private double lon;
-	private Vector <DataWeather> dataweather= new Vector <>();
+	private long time;
 	
 	
-	public City (long id, String country, String name,double lat,double lon) {
+	public City (long id, String country, String name,double lat,double lon, long time) {
 		this.id = id;
 		this.country = country;
 		this.name = name;
 		this.lat=lat;
 		this.lon=lon;
+		this.time=time;
 	}
 
 	public double getId() {
@@ -59,12 +72,21 @@ public class City {
 	public void setLon(double lon) {
 		this.lon = lon;
 	}
+	
+	public void setTime(long time) {
+		this.time = time;
+	}
+	
+	public long getTime(long time) {
+		return time;
+	}
 
-	public Vector<DataWeather> getDataweather() {
+	/*public Vector<DataWeather> getDataweather() {
 		return dataweather;
 	}
 
 	public void setDataweather(Vector<DataWeather> dataweather) {
 		this.dataweather = dataweather;
 	}
+	*/ //TODO Servono davvero?
 }
