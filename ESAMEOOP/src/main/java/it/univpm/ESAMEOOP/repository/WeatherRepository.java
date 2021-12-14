f9d933b6c3dd2219089cbe2c3a1d4602e751c537
DaniloLP01 <danilola.palombara@gmail.com>
2021-12-14 13:15:39 +0100

    Realizzazione repository

--- ESAMEOOP/src/main/java/it/univpm/ESAMEOOP/repository/WeatherRepository.java
+++ ESAMEOOP/src/main/java/it/univpm/ESAMEOOP/repository/WeatherRepository.java
@@ -10,10 +10,10 @@ import it.univpm.ESAMEOOP.model.City;
 public interface WeatherRepository extends CrudRepository<City, long> {
 	
 	List<City> FindById (long id);
-	
+		
 	@Query(value = "SELECT * FROM Meteo WHERE ID = :id AND EPOCH >= :start AND EPOCH <= :stop", nativeQuery = true)
 	List<City> GetValue(@Param("id") long id, @Param("start") Long start, @Param("stop") Long stop);
-	
+		
 	City savedCity = WeatherRepository.save(city); //TODO dove va?
 	
 }
