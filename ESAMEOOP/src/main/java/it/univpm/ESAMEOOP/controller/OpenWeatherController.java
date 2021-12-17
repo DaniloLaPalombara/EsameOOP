590341818f21951940bb371e83865ae014845348
NicoloIanni <nicoloianni01@gmail.com>
2021-12-17 16:58:12 +0100

    Creazione controller

--- ESAMEOOP/src/main/java/it/univpm/ESAMEOOP/controller/OpenWeatherController.java
+++ ESAMEOOP/src/main/java/it/univpm/ESAMEOOP/controller/OpenWeatherController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import it.univpm.ESAMEOOP.service.OpenWeatherService;
 
@@ -28,5 +29,12 @@ public class OpenWeatherController {
 	public ResponseEntity <Object> getforecas()
 	{
 		return new ResponseEntity<>(openweatherservice.getDataWeather(3183087),HttpStatus.OK);
-	}			
+	}
+	
+	@RequestMapping(value = "/CurrentWeather")
+	public ResponseEntity <Object> getforecastHistory(@RequestParam(name="id",defaultValue="3183087")Integer id)
+	{
+		return new ResponseEntity<>(openweatherservice.createJSON(openweatherservice.setDataWeather(openweatherservice.getDataWeather(id))),HttpStatus.OK);
+	}
+	
 }
